a49fa2fd5eefd924ba974783c13d8c3e039ca7da
Calvin Pelletier <calvin.pelletier@gmail.com>
2017-04-05 19:21:15 -0500

    fixed coinbase error

--- MP3/MP3-Release/src/main/DatasetGenerator.java
+++ MP3/MP3-Release/src/main/DatasetGenerator.java
@@ -15,22 +15,22 @@ public class DatasetGenerator {
 	}
 
 	public boolean writeTransactions() {
-		
+
 		//long tx = getIndex()
-		
+
 		BufferedWriter bw = null;
 		try {
 			bw = new BufferedWriter(new FileWriter(file));
 		} catch (IOException x){
 			System.err.format("IOException");
 			return false;
-		} 
+		}
 
-		
+
 		BlockExplorer be = new BlockExplorer();
 
 		List<Block> lb = new ArrayList<Block>();
-		
+
 		try{
 			for (long i = 265852; i <= 266085; i++)
 				lb.addAll(be.getBlocksAtHeight(i));
@@ -42,25 +42,24 @@ public class DatasetGenerator {
 		System.out.println("Blocks fetched");
 		for (Block j : lb){
 			List<Transaction> t = j.getTransactions();
-			
+
 			for (Transaction i : t){
 				boolean isBase = false;
 				List<Input> in   = i.getInputs();
 				List<Output> out = i.getOutputs();
 				String hash = i.getHash();
-				long idx = i.getIndex();	
-				//Coinbase checking is not working right 
-				/*
+				long idx = i.getIndex();
+
 				for (Input k : in){
 					if (k.getPreviousOutput() == null && in.size() == 1){
 						isBase = true;
 						break;
 					}
-						
+
 				}
 				if (isBase == true)
-					break;
-					*/
+					continue;
+
 				try {
 					for (Input k : in){
 						if (k.getPreviousOutput() == null)
@@ -71,11 +70,11 @@ public class DatasetGenerator {
 					for (Output l : out){
 						bw.write(generateOutputRecord(idx, hash, l.getAddress(), l.getValue()));
 						bw.newLine();
-					}	
+					}
 				} catch (IOException x){
 					System.err.format("IOException");
 					return false;
-				} 
+				}
 			}
 		}
 		/**/
@@ -84,14 +83,14 @@ public class DatasetGenerator {
 		}
 		catch (IOException ex){
 			System.err.format("IOException");
-			return false;	
+			return false;
 		}
 		return true;
 	}
 
 	/**
 	 * Generate a record in the transaction dataset
-	 * 
+	 *
 	 * @param txIndex
 	 *            Transaction index
 	 * @param txHash
@@ -109,7 +108,7 @@ public class DatasetGenerator {
 
 	/**
 	 * Generate a record in the transaction dataset
-	 * 
+	 *
 	 * @param txIndex
 	 *            Transaction index
 	 * @param txHash
